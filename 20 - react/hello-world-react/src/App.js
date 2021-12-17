cf3be0f08634030170e116ea0ead652b94556de3
Maicon Souza <maicon_souza09@hotmail.com>
2021-12-16 22:30:02 -0300

    react - components & props

--- 20 - react/hello-world-react/src/App.js
+++ 20 - react/hello-world-react/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import Header from './Header'
+
+/*
+COMPONENTES - Criando e Usando React
+
+-Import React
+-Função que retorna JSX
+    -Sempre tem que ter um elemento pai
+    -React Fragment
+-Exportar Função
+-Para usar o Componente
+    -Importar
+    -Usar como tag, exemplo <App />
+
+*/
 
 function App(){
-    return (
-        <div>
-            <h1>Hello World!</h1>
-        </div>
-    )
+    return <>
+            <Header titulo="Título" subtitulo="Subtitulo"/>
+
+        </>
+    
 }
 
 export default App
---
+++ 20 - react/hello-world-react/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header({titulo, subtitulo}){
+    return (
+       
+            <header>
+                <h1>{titulo}</h1>
+                <h2>{subtitulo}</h2>
+            </header>
+        
+    )
+}
+
+export default Header
