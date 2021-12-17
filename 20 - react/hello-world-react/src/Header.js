589fbfafae9f5886c58f7e948100048b340a129e
Maicon Souza <maicon_souza09@hotmail.com>
2021-12-17 20:41:35 -0300

    react - add css aos componentes

--- 20 - react/hello-world-react/src/Header.js
+++ 20 - react/hello-world-react/src/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './Header.css'
 
 function Header({titulo, subtitulo}){
     return (
        
-            <header>
+            <header className="header">
                 <h1>{titulo}</h1>
                 <h2>{subtitulo}</h2>
             </header>
