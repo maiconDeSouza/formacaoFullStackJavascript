402c2f0b95664ef9572551f6b3fedf06c33cc314
Maicon Souza <maicon_souza09@hotmail.com>
2021-12-19 14:22:53 -0300

    react - novo projeto contador - para estudar navegacao

--- 20 - react/02-contador/react-counter/src/App.js
+++ 20 - react/02-contador/react-counter/src/App.js
@@ -1,11 +1,46 @@
-import React from "react"
+import React, {useState} from "react"
 import Counter from './Counter'
+import Albums from './Albums'
+import Users from './Users'
+
+
+
 
 
 function App() {
+  const [page, setPage] = useState()
+
+  const handleChangePage = (e) => {
+    const page = e.target.innerText
+    setPage(page)
+  }
+
+  const pages = {
+    'Albums': Albums,
+    'Counter': Counter,
+    'Users': Users
+
+  }
+
+  const Page = pages[page] || Counter
+
+  // const arrayPages = Object.keys(pages)
+
   return (
     <>
-      <Counter />
+
+      {/* {
+        Object.keys(pages).map(item =>{
+          return <button onClick={handleChangePage}>{item}</button>
+        })
+      } */}
+     
+      <button onClick={handleChangePage}>Albums</button>
+      <button onClick={handleChangePage}>Counter</button>
+      <button onClick={handleChangePage}>Users</button>
+
+
+      <Page />
     </>
   )
 }
