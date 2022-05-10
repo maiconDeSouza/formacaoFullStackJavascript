f1a126c5e907b0abbe41f08dbba08dd18db7b33e
Maicon Souza <maicon_souza09@hotmail.com>
2022-05-10 17:37:00 -0300

    add metodo get

--- 19-api-restful/src/routes/index.js
+++ 19-api-restful/src/routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const ProductsControllers = require('../controllers/products')
 
 
-router.get('/products', ProductsControllers.get)
+router.get('/products/:id?', ProductsControllers.get)
 router.post('/products', ProductsControllers.post)
 router.put('/products/:id', ProductsControllers.put)
 router.delete('/products/:id', ProductsControllers.del)
