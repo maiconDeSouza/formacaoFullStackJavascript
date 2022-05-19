dfc678621c8b92715c70f71922f9d23c6d58922c
Maicon Souza <maicon_souza09@hotmail.com>
2022-05-18 21:29:18 -0300

    useState

---
+++ 20-react/react-counter/src/Counter.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './Counter.css'
+
+export function Counter(){
+    const [contador, setContador] = useState(0)
+
+    const handleOnclick = op =>{
+        if(op === '+'){return setContador(contador + 1)}
+        if(op === '-' && contador > 0){return setContador(contador - 1)}
+    }
+    return (
+        <div className='counter'>
+            <span>{contador}</span>
+            <button onClick={() => handleOnclick('+')}>+</button>
+            <button onClick={() => handleOnclick('-')}>-</button>
+        </div>
+    )
+}
